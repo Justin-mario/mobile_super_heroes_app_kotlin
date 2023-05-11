2d47121ca0549666fe2a4c4104d400098da06a23
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2023-05-11 13:13:22 +0100

    third commit

--- app/src/main/java/com/example/android/superheroesapp/service/HeroesScreen.kt
+++ app/src/main/java/com/example/android/superheroesapp/service/HeroesScreen.kt
@@ -1,6 +1,14 @@
 package com.example.android.superheroesapp.service
 
-import androidx.annotation.StringRes
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,17 +16,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -28,16 +40,50 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.android.superheroesapp.R
 import com.example.android.superheroesapp.model.Hero
-import com.example.android.superheroesapp.model.HeroesRepository
+import com.example.android.superheroesapp.model.HeroesRepository.heroes
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
 @Composable
 fun HeroesApp(modifier: Modifier = Modifier ) {
     Scaffold(topBar = { HeroTopAppBar()}) {
-        LazyColumn(contentPadding = it, modifier = modifier){
-            items(HeroesRepository.heroes) { hero ->
-                HeroItem(hero = hero, modifier = Modifier.padding(8.dp))
+        val visibleState = remember {
+            MutableTransitionState(false).apply {
+                // Start the animation immediately.
+                targetState = true
             }
+        }
+
+        // Fade in entry animation for the entire list
+        AnimatedVisibility(
+            visibleState = visibleState,
+            enter = fadeIn(
+                animationSpec = spring(dampingRatio = Spring.DampingRatioLowBouncy)
+            ),
+            exit = fadeOut(),
+            modifier = modifier
+        ) {
+        LazyColumn(contentPadding = it, modifier = modifier){
+            itemsIndexed(heroes) { index, hero ->
+                HeroItem(
+                    hero = hero,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                        // Animate each list item to slide in vertically
+                        .animateEnterExit(
+                            enter = slideInVertically(
+                                animationSpec = spring(
+                                    stiffness = Spring.StiffnessVeryLow,
+                                    dampingRatio = Spring.DampingRatioLowBouncy
+                                ),
+                                initialOffsetY = { it * (index + 1) } // staggered entrance
+                            )
+                        )
+                )
+            }
+        }
+//            items(HeroesRepository.heroes) { hero ->
+//                HeroItem(hero = hero, modifier = Modifier.padding(8.dp))
+//            }
 
         }
     }
@@ -59,22 +105,34 @@ fun HeroTopAppBar(modifier: Modifier = Modifier){
 @Composable
 fun HeroItem(hero: Hero, modifier: Modifier = Modifier) {
     Card(modifier = modifier
-        .fillMaxSize()
-        .height(86.dp),
-
+        .fillMaxSize(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 12.dp),
         shape = MaterialTheme.shapes.medium,
     ) {
         Row(modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .padding(16.dp)
+            .sizeIn(minHeight = 72.dp),
         ) {
-            HeroInfo(heroName = hero.nameRes, heroDescription = hero.descriptionRes)
-            Spacer(modifier = Modifier.weight(1f))
-            Box(modifier = Modifier.height(72.dp)){
+            Column(modifier = modifier.weight(1F)) {
+                Text(text = stringResource(id = hero.nameRes),
+                    style = MaterialTheme.typography.displaySmall,)
+                Text(text = stringResource(id = hero.descriptionRes),
+                    style = MaterialTheme.typography.bodyLarge, modifier = Modifier,
+
+                    )
+
+            }
+
+            Spacer(Modifier.width(16.dp))
+            Box(modifier = Modifier
+                .size(72.dp)
+                .clip(MaterialTheme.shapes.small),
+                ){
                 Image(painter = painterResource(id = hero.imageRes), contentDescription = stringResource(
-                    id = hero.nameRes), modifier = Modifier
-                    .clip(MaterialTheme.shapes.small),
-                    contentScale = ContentScale.FillHeight)
+                    id = hero.nameRes), modifier = Modifier,
+                    alignment = Alignment.TopCenter,
+                    contentScale = ContentScale.FillWidth)
 
             }
         }
@@ -82,18 +140,7 @@ fun HeroItem(hero: Hero, modifier: Modifier = Modifier) {
     }
 }
 
-@Composable
-fun HeroInfo(@StringRes heroName: Int, @StringRes heroDescription: Int, modifier: Modifier = Modifier){
-    Column(modifier = modifier) {
-        Text(text = stringResource(id = heroName),
-            style = MaterialTheme.typography.displaySmall,)
-        Text(text = stringResource(id = heroDescription),
-            style = MaterialTheme.typography.bodyLarge, modifier = Modifier,
 
-            )
-
-    }
-}
 
 //@Composable
 //fun HeroList(){
